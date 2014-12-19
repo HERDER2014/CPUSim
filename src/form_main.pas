dae44b5a6b8199ee71a8503dd2eb1206ac7f8bc9
LeonardKoenig <leonard@localhost.localdomain>
2014-12-19 09:29:28 +0100

    added basic Open/Save dialog

--- src/form_main.pas
+++ src/form_main.pas
@@ -16,8 +16,16 @@ type
     Button1: TButton;
     MainFrm_Menu: TMainMenu;
     MainFrm_Menu_File: TMenuItem;
+    MainFrm_Menu_File_Open: TMenuItem;
+    MainFrm_Menu_File_Save: TMenuItem;
+    MainFrm_Menu_File_SaveAs: TMenuItem;
+    MenuItem1: TMenuItem;
+    OpenDlg: TOpenDialog;
+    SaveDlg: TSaveDialog;
     procedure Button1Click(Sender: TObject);
     procedure FormCreate(Sender: TObject);
+    procedure MainFrm_Menu_File_OpenClick(Sender: TObject);
+    procedure MainFrm_Menu_File_SaveClick(Sender: TObject);
   private
     { private declarations }
   public
@@ -35,7 +43,17 @@ implementation
 
 procedure TmainFrm.FormCreate(Sender: TObject);
 begin
+  //OpenDlg.Create(OpenDlg);
+end;
 
+procedure TmainFrm.MainFrm_Menu_File_OpenClick(Sender: TObject);
+begin
+  OpenDlg.execute;
+end;
+
+procedure TmainFrm.MainFrm_Menu_File_SaveClick(Sender: TObject);
+begin
+  SaveDlg.Execute;
 end;
 
 procedure TmainFrm.Button1Click(Sender: TObject);

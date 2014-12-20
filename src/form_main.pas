8e6e296ebc5aeda5865a603c4d8c390070ae46d8
LeonardKoenig <leonard@localhost.localdomain>
2014-12-20 16:11:41 +0100

    (Re-)Added Dialogs, exit app, removed some test stuff

--- src/form_main.pas
+++ src/form_main.pas
@@ -15,34 +15,37 @@ type
   TmainFrm = class(TForm)
     MainFrm_Menu: TMainMenu;
     MainFrm_Menu_File: TMenuItem;
-    Memo1: TMemo;
-    MenuItem1: TMenuItem;
-    MenuItem10: TMenuItem;
-    MenuItem11: TMenuItem;
-    MenuItem12: TMenuItem;
-    MenuItem13: TMenuItem;
-    MenuItem14: TMenuItem;
-    MenuItem15: TMenuItem;
-    MenuItem16: TMenuItem;
-    MenuItem17: TMenuItem;
-    MenuItem18: TMenuItem;
-    MenuItem19: TMenuItem;
-    MenuItem2: TMenuItem;
-    MenuItem20: TMenuItem;
-    MenuItem21: TMenuItem;
-    MenuItem22: TMenuItem;
-    MenuItem23: TMenuItem;
-    MenuItem24: TMenuItem;
-    MenuItem3: TMenuItem;
-    MenuItem4: TMenuItem;
-    MenuItem5: TMenuItem;
-    MenuItem6: TMenuItem;
-    MenuItem7: TMenuItem;
-    MenuItem8: TMenuItem;
-    MenuItem9: TMenuItem;
+    MainFrm_Menu_File_New: TMenuItem;
+    MainFrm_Menu_Edit: TMenuItem;
+    MainFrm_Menu_Edit_Undo: TMenuItem;
+    MainFrm_Menu_Edit_Redo: TMenuItem;
+    MainFrm_Menu_Edit_Spacer1: TMenuItem;
+    MainFrm_Menu_Edit_Cut: TMenuItem;
+    MainFrm_Menu_Edit_Copy: TMenuItem;
+    MainFrm_Menu_Edit_Paste: TMenuItem;
+    MainFrm_Menu_Edit_Spacer2: TMenuItem;
+    MainFrm_Menu_Edit_Find: TMenuItem;
+    MainFrm_Menu_Edit_FindNext: TMenuItem;
+    MainFrm_Menu_File_Spacer1: TMenuItem;
+    MainFrm_Menu_Help: TMenuItem;
+    MainFrm_Menu_Help_Contents: TMenuItem;
+    MainFrm_Menu_Help_Tutorial: TMenuItem;
+    MainFrm_Menu_Help_Spacer1: TMenuItem;
+    MainFrm_Menu_Help_About: TMenuItem;
+    MainFrm_Menu_File_Open: TMenuItem;
+    MainFrm_Menu_File_OpenRecent: TMenuItem;
+    MainFrm_Menu_File_Save: TMenuItem;
+    MainFrm_Menu_File_SaveAs: TMenuItem;
+    MainFrm_Menu_File_Close: TMenuItem;
+    MainFrm_Menu_File_Spacer2: TMenuItem;
+    MainFrm_Menu_File_Exit: TMenuItem;
     OpenDlg: TOpenDialog;
     SaveDlg: TSaveDialog;
+    procedure Button1Click(Sender: TObject);
     procedure FormCreate(Sender: TObject);
+    procedure MainFrm_Menu_File_ExitClick(Sender: TObject);
+    procedure MainFrm_Menu_File_OpenClick(Sender: TObject);
+    procedure MainFrm_Menu_File_SaveClick(Sender: TObject);
   private
     { private declarations }
   public
@@ -60,7 +63,29 @@ implementation
 
 procedure TmainFrm.FormCreate(Sender: TObject);
 begin
-  //OpenDlg.Create(OpenDlg);
+
+end;
+
+procedure TmainFrm.MainFrm_Menu_File_ExitClick(Sender: TObject);
+begin
+  Application.Terminate;
+end;
+
+procedure TmainFrm.Button1Click(Sender: TObject);
+begin
+  //ApplicationProperties1.Create(mainFrm);
+//  ApplicationProperties1.;
+end;
+
+
+procedure TmainFrm.MainFrm_Menu_File_OpenClick(Sender: TObject);
+begin
+  OpenDlg.Execute;
+end;
+
+procedure TmainFrm.MainFrm_Menu_File_SaveClick(Sender: TObject);
+begin
+  SaveDlg.Execute;
 end;
 
 

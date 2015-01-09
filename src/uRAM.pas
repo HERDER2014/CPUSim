db8997dbdafe5b219fac111337ac7e998307a9b6
uderde <dennis.uderhardt@t-online.de>
2015-01-09 09:08:17 +0100

    init2

--- src/uRAM.pas
+++ src/uRAM.pas
@@ -15,7 +15,6 @@ type TRAM = class
     Effekt: -
     Ergebnis: Liefert das Byte an Adresse addr.
   }
-
   function ReadByte(addr : Cardinal) : Byte;
 
   { Vor.: addr < size
@@ -36,29 +35,46 @@ type TRAM = class
   }
   procedure WriteWord(addr : Cardinal; w : Word);
 
+  { Vor.: RAM ist kreiert
+    Effekt: -
+    Ergebnis: Gibt Größe des RAM aus.
+  }
+  function GetSize : Cardinal;
 end;
 
 implementation
+var ram : ARRAY OF Word;
+var size_RAM : Cardinal;
+
    constructor TRAM.Create(size : Cardinal);
    begin
-
+     SetLength(ram, size);
+     size_RAM := size;
    end;
+
    function TRAM.ReadByte(addr : Cardinal) : Byte;
    begin
-
+     if addr < size_RAM then result := ram[addr];
    end;
+
    procedure TRAM.WriteByte(addr : Cardinal; b : Byte);
    begin
-
+     if addr < size_RAM then ram[addr] := b;
    end;
+
    function TRAM.ReadWord(addr : Cardinal) : Word;
    begin
-
+     if addr < (size_RAM - 1) then result := ram[addr];
    end;
+
    procedure TRAM.WriteWord(addr : Cardinal; w : Word);
    begin
+     if addr < (size_RAM - 1) then ram[addr] := w;
+   end;
 
+   function TRAM.GetSize : Cardinal;
+   begin
+     result := size_RAM;
    end;
 
 end.
-

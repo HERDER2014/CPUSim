8eef036b0162e325b869bd102065abbcdb40f465
LeonardKoenig <leonard.r.koenig@googlemail.com>
2015-01-16 08:53:41 +0100

    manually merged stuff

--- src/uTypen.pas
+++ src/uTypen.pas
@@ -7,27 +7,34 @@ interface
 uses
   Classes, SysUtils;
 
-   type RegisterIndex = (AX, BX, CX, DX,
-                         AL, BL, CL, DL,
-                         AH, BH, CH, DH,
-                         BP, SP, IP, FLAGS);
+type
+  RegisterIndex = (AX, BX, CX, DX,
+    AL, BL, CL, DL,
+    AH, BH, CH, DH,
+    BP, SP, IP, FLAGS,
 
-   type TRegRecord = Record
-      AX : Word;
-      BX : Word;
-      CX : Word;
-      DX : Word;
+    {
+     Wird vom Compiler dazu genutzt, ungültige Register-
+     Eingaben zu identifizieren.
+    }
+    INVALID);
 
-      BP : Word;
-      SP : Word;
-      IP : Word;
+type
+  TRegRecord = record
+    AX: word;
+    BX: word;
+    CX: word;
+    DX: word;
 
-      FLAGS : Word;
-   end;
+    BP: word;
+    SP: word;
+    IP: word;
 
-   type TFlags = (O=2048,S=128,Z=64);
+    FLAGS: word;
+  end;
+
+  type TFlags = (O=2048,S=128,Z=64);
 
 implementation
 
 end.
-

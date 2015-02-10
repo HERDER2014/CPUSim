b9159ca12b88405c9c4e24d82f7d49c212d6763d
jacobsjo <JochenJacobs33@gmail.com>
2015-02-10 01:02:58 +0100

    added velocityTester

---
+++ Programme/velocityTester.asm
@@ -0,0 +1,15 @@
+; This Program can be used to
+; determine the maximum velocity
+; of the simulation. The total
+; count of executed OP-Codes is
+; 33555203. ((2*16^4+3)*16^2+3)
+
+MOV AX,FFFF
+MOV BX,FF
+start:
+SUB AX,01
+JNZ start
+MOV AX,FFFF
+SUB BX,01
+JNZ start
+END
